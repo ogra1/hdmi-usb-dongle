5038302792e5b0a1a5384ffa97685441d6a7249e
Oliver Grawert <ogra@ubuntu.com>
2021-08-16 15:37:56 +0200

    add auto-detection for video device

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 
 # set some defaults
-DEVICE="/dev/video2"
 WIDTH="1280"
 HEIGHT="720"
 FPS="30"
@@ -20,6 +19,18 @@ else
 # FMT="mjpg"
 EOF
 fi
+
+find_device () {
+	VENDORS="534d" # add more vendor IDs here
+	DEV="$(for vendor in $VENDORS; do for file in /sys/bus/usb/devices/*/idVendor; do \
+		if grep -q $vendor $file 2>/dev/null; then \
+			ls $(echo $file| sed 's/idVendor/*/')/video4linux; \
+		fi; \
+	done; done | sort | head -1)"
+	echo $DEV
+	}
+
+[ -n "$DEVICE" ] || DEVICE="/dev/$(find_device)"
 
 # run mplayer with options from config file
 $SNAP/usr/bin/mplayer -ao pulse tv:// \
