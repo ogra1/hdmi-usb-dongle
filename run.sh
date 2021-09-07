32ec7dd45e2948954c9ef8f3c342ef91c00ee0dd
Oliver Grawert <ogra@ubuntu.com>
2021-09-07 15:15:22 +0200

    refactor and clean up the code

--- run.sh
+++ run.sh
@@ -28,36 +28,50 @@ else
 EOF
 fi
 
-find_device () {
-	VENDORS="534d eba4" # add more vendor IDs here
-	DEV="$(for vendor in $VENDORS; do for file in /sys/bus/usb/devices/*/idVendor; do \
-		if grep -q $vendor $file 2>/dev/null; then \
-		  echo -n "$vendor "; ls $(echo $file| sed 's/idVendor/*/')/video4linux; \
-		fi; \
-	done; done | sort | head -1)"
-	echo "$DEV"
-	}
+VENDORS="534d eba4" # add more vendor IDs here
 
-[ -n "$DEVICE" ] || DEVICE="/dev/$(find_device | cut -d ' ' -f2)"
+if snapctl is-connected hardware-observe; then
+  # find audio and video devices by vendor id
+  for vendor in $VENDORS; do
+    VIDEODEV="$(for file in /sys/bus/usb/devices/*/idVendor; do \
+        if grep -q $vendor $file 2>/dev/null; then \
+          ls $(echo $file| sed 's/idVendor/*/')/video4linux; \
+        fi; \
+      done | sort | head -1)"
+    AUDIODEV="$(LC_ALL=C pactl list sources | \
+      sed -n '/^.*Name:.*$/p;/^.*device.vendor.id.*$/p' | \
+      grep -B1 $vendor | head -1 | sed 's/^.*Name: //')"
+    # stop at the first device found
+    [ -n "$VIDEODEV" ] && break
+  done
+else
+  echo "Please connect the hardware-observe snap plug to make device detection possible"
+  exit 1
+fi
+
+# allow user to override the video device
+[ -n "$DEVICE" ] || DEVICE="/dev/${VIDEODEV}"
 
 # show a fancy tray icon with menu
 PYTHONPATH=$SNAP/lib/python3.8/site-packages:$SNAP/gnome-platform/usr/lib/python3.8/site-packages $SNAP/usr/bin/trayicon &
 
-# make audio work (poor man's loopback monitor with two piped pacat commands)
 if snapctl is-connected audio-record; then
-	case $(find_device | cut -d ' ' -f1) in
-		534d)
-			MATCH="MACROSILICON"
-			;;
-		eba4)
-			MATCH="Video_Capture_HU123450"
-			;;
-	esac
-	AUDIODEV="$(pactl list sources|grep Name|grep $MATCH|sed 's/^.*: //')"
-	pacat -r --device="$AUDIODEV" --latency-msec=1 | pacat -p --latency-msec=1 &
+    # make audio work (poor man's loopback monitor with two piped pacat commands)
+	if [ -n "$AUDIODEV" ]; then
+	  pacat -r --device="$AUDIODEV" --latency-msec=1 | pacat -p --latency-msec=1 &
+	else
+      echo "No matching audio device found, moving on without sound !!"
+    fi
+else
+	echo "Please connect the audio-record snap plug to get audio support"
 fi
 
-# run mplayer with options from config file
-$SNAP/usr/bin/mplayer -ao pulse tv:// \
-	-tv driver=v4l2:device=${DEVICE}:width=${WIDTH}:height=${HEIGHT}:fps=${FPS}:outfmt=${FMT} \
-	-title "HDMI USB Dongle" >/dev/null 2>&1
+if snapctl is-connected camera; then
+  # run mplayer with options from config file and detection
+  $SNAP/usr/bin/mplayer -ao pulse tv:// \
+    -tv driver=v4l2:device=${DEVICE}:width=${WIDTH}:height=${HEIGHT}:fps=${FPS}:outfmt=${FMT} \
+    -title "HDMI USB Dongle" >/dev/null 2>&1
+else
+  echo "Please connect the camera snap plug to get access to the video device"
+  exit 1
+fi
